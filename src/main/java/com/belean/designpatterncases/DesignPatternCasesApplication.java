3d2ce0b6a6f2a8d227a54281ab60f984f26d875f
belean <liuconglook@gmail.com>
2021-10-20 16:19:28 +0800

    update name

--- src/main/java/com/belean/designpatterncases/DesignPatternCasesApplication.java
+++ src/main/java/com/belean/designpatterncases/DesignPatternCasesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesignModeCasesApplication {
+public class DesignPatternCasesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesignModeCasesApplication.class, args);
+        SpringApplication.run(DesignPatternCasesApplication.class, args);
     }
 
 }
